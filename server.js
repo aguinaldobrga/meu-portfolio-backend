fcd1f295d2b456ff219136d018d909c5187e6e21
aguinaldobrga <guigo.abf@gmail.com>
2025-08-29 20:53:44 -0400

    commit inicial do backend

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+require("dotenv").config();
+
+// O objeto 'app' precisa ser criado primeiro
+const app = express();
+const port = 3001;
+
+// Middleware para processar JSON e habilitar CORS
+app.use(express.json());
+app.use(cors());
+
+// --- ROTAS DA API ---
+
+// Importe os dados dos projetos e defina a rota aqui
+const projectsData = require("./projects.json");
+
+app.get("/api/projects", (req, res) => {
+  res.json(projectsData);
+});
+
+// Configuração do Nodemailer
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+// Rota para o envio do formulário
+app.post("/send-email", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: `Nova mensagem de contato de: ${name}`,
+    text: `
+    Nome: ${name}
+    Email: ${email}
+    Mensagem: ${message} `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .send({ success: false, message: "Erro ao enviar a mensagem." });
+    }
+    res
+      .status(200)
+      .send({ success: true, message: "Mensagem enviada com sucesso!" });
+  });
+});
+
+// --- INICIALIZAÇÃO DO SERVIDOR ---
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
